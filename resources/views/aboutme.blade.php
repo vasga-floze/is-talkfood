6b93948f6b35f8124ce8d8f52ba3b106f9b1d643
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-06-03 01:16:59 -0600

    Se agrega contenido a vista about me

--- resources/views/aboutme.blade.php
+++ resources/views/aboutme.blade.php
@@ -1,20 +1,45 @@
 <x-app-layout>
     <x-slot name="header">
         <form class="form-inline my-4 my-lg-0">
-            <a class="nav-link" href="{{ url('/aboutme') }}">About Me</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <a class="nav-link mx-auto" href="{{ url('/aboutme') }}"><span>About Me</span></a>
 
-            <a class="nav-link" href="{{ url('/favorites') }}">Favorites</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <a class="nav-link mx-auto" href="{{ url('/favorites') }}"><span>Favorites</span></a>
 
-            <a class="nav-link" href="{{ url('/recipebook') }}">Recipe Book</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <a class="nav-link mx-auto" href="{{ url('/recipebook') }}"><span>Recipe Book</span></a>
         </form>
     </x-slot>
 </x-app-layout>
+<h2 class="text-center">ABOUT ME</h2>
+<div class="card">
+    <div class="card-body px-5 py-3 border-bottom rounded-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card" style="width: 36rem; height: 150px;">
+                        <div class="card-body">
+                            <h5 class="card-title">ABOUT ME</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card" style="width: 10rem; height: 150px;">
+                        <img src="{{asset('images/profile.png')}}" width="150" height="125">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card" style="width: 36rem; height: 150px;">
+                        <div class="col align-self-center">
+                            <h5 class="card-title">MY LOCATION</h5>
+                        </div>
+                    <div>
+                </div> 
+            </div>
+        </div>
+    </div>
+</div>
+
