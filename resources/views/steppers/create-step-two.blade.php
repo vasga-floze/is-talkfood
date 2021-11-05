4b082c7a82ce34522a858faf1bffc5f6501a4bc8
vasga-floze <gabriela.floze@gmail.com>
2021-11-05 04:17:53 -0600

    agrega logica de diseño para el registro de pasos en una receta

--- resources/views/steppers/create-step-two.blade.php
+++ resources/views/steppers/create-step-two.blade.php
@@ -22,23 +22,47 @@
                             @endif
 
                            
-
-
                             <div class="form-group text-center">
-                                <label for="receta">Agregar paso de la receta</label>
-                                <input  class="form-control" id="receta" required>
+                                <label for="receta">Agregar pasos de la receta (uno por uno, enter para agregarlo)</label>
+                                <textarea  class="form-control" id="receta" required></textarea>
                             </div>
-  
-  
-                         
+
+                            <div>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                        <th scope="col"><!--El ID no se muestra--></th>
+                                        <th scope="col">Paso</th>
+                                        <th scope="col">Indicación</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row"><!--1--></th>
+                                        <td>1</td>
+                                        <td>Vertir el agua en la harina y mezclar</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row"><!--2--></th>
+                                        <td>2</td>
+                                        <td>Aplicar aceite en aerosol en un sarten y ponerlo en la estufa a fuego medio</td>
+                                        </tr>
+                                        <tr>
+                                        <th scope="row"><!--3--></th>
+                                        <td>3</td>
+                                        <td>Hacer bolitas con la masa resultante en el paso 1</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
 
                             <center>
-                                <div class="col-5 mb-3">
+                                <hr>
+                                <div class="col-5 mb-3 py-5">
                                     <label for="tiempo">Tiempo de la receta</label>
                                     <input  class="form-control" id="tiempo" placeholder="30min" required> 
                                 </div>
                             </center>
-
                             
                     </div>
                     <div class="card-footer">
